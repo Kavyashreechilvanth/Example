194e21e17351aa626e22dcab370419136c9f8858
kavyashree chilvanth <kavyashreechilvanth@gmail.com>
2023-12-14 13:44:06 +0530

    Created class called user with username and id attribute

---
+++ src/com/jsp/example/User.java
@@ -0,0 +1,19 @@
+package com.jsp.example;
+
+class User {
+	private String username;
+	private int id;
+	public void setUsername() {
+		this.username=username;
+	}
+	public void setId() {
+		this.id=id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public int getId() {
+		return id;
+	}
+
+}
